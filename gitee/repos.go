155739805c6ce41ebfc330cc2e78e9f096925cd6
bright.ma <bright.ma@blackshark.com>
2022-10-27 15:15:44 +0800

    gitee: repos.go: add repos.go file
    Change-Id: Ic2b3d551a7103135eece521ff6d58d7af606266c

---
+++ gitee/repos.go
@@ -0,0 +1,7 @@
+package gitee
+
+// RepositoriesService handles communication with the repository related
+// methods of the gitee API.
+//
+// gitee API docs: https://gitee.com/api/v5/repos
+type RepositoriesService service
