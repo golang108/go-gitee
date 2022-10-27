f32bb7062e84aabaa4c9f422305aea973d485e75
bright.ma <bright.ma@blackshark.com>
2022-10-27 17:26:21 +0800

    test: repos_test.go: add  func TestListCommits
    Change-Id: I0f55075c71f5bb0da15c0890042a5de2018793a8

--- test/repos_test.go
+++ test/repos_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"fmt"
+	"github.com/mamh-mixed/go-gitee/gitee"
 	"testing"
 )
 
@@ -18,5 +19,24 @@ func TestGetCommit(t *testing.T) {
 	commit, response, err := client.Repositories.GetCommit(ctx, "mamh-mixed", "go-gitee", "8896821c53eda6698ef5c75ba5182e547e8476f1")
 
 	fmt.Println(commit, response, err)
+
+}
+
+func TestListCommits(t *testing.T) {
+	var opts = &gitee.CommitsListOptions{}
+	for {
+		commits, response, err := client.Repositories.ListCommits(ctx, "mamh-mixed", "go-gitee", opts)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for index, commit := range commits {
+			fmt.Println(index, len(commits), *commit.Commit.Message, *commit.SHA)
+		}
+		if response.NextPage == 0 {
+			break
+		}
+		opts.Page = response.NextPage
+	}
 
 }
