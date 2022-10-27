2879bc070406ccac1a2d43972c9320fa3bf659e5
bright.ma <bright.ma@blackshark.com>
2022-10-27 15:35:51 +0800

    test: repos_test.go: update func TestListBranches
    Change-Id: Iec184657e4bc755d8e0ed79dc5f080d264ea9beb

--- test/repos_test.go
+++ test/repos_test.go
@@ -6,8 +6,10 @@ import (
 )
 
 func TestListBranches(t *testing.T) {
-	repos, response, err := client.Repositories.ListBranches(ctx, "mamh-java", "jenkins-jenkins")
-	fmt.Println(repos)
+	branches, response, err := client.Repositories.ListBranches(ctx, "mamh-java", "jenkins-jenkins")
+	for index, br := range branches {
+		fmt.Println(index, *br.Name, *br.Protected, *br.ProtectionUrl)
+	}
 	fmt.Println(response)
 	fmt.Println(err)
 }
