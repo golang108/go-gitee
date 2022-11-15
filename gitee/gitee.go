c5d1f1c780d71b3687ebfd3cadaa6cbce683f637
bright.ma <bright.ma@blackshark.com>
2022-11-15 11:28:08 +0800

    gitee: gitee.go: add func parseBoolResponse
    Change-Id: I40f534dcb520ba1c5ad68ffc9fc01b6fe11ec4c8
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- gitee/gitee.go
+++ gitee/gitee.go
@@ -292,6 +292,25 @@ func CheckResponse(r *http.Response) error {
 	}
 }
 
+// parseBoolResponse determines the boolean result from a API response.
+// Several API methods return boolean responses indicated by the HTTP
+// status code in the response (true indicated by a 204, false indicated by a
+// 404). This helper function will determine that result and hide the 404
+// error if present. Any other error will be returned through as-is.
+func parseBoolResponse(err error) (bool, error) {
+	if err == nil {
+		return true, nil
+	}
+
+	if err, ok := err.(*ErrorResponse); ok && err.Response.StatusCode == http.StatusNotFound {
+		// Simply false. In this one case, we do not pass the error through.
+		return false, nil
+	}
+
+	// some other real error occurred
+	return false, err
+}
+
 // BareDo sends an API request and lets you handle the api response. If an error
 // or API Error occurs, the error will contain more information. Otherwise you
 // are supposed to read and close the response's Body. If rate limit is exceeded
