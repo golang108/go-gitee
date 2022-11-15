6070c4939b0568cca7dd76c0d73484170133888b
bright.ma <bright.ma@blackshark.com>
2022-11-15 17:47:13 +0800

    gitee: users.go: rename GetUserKeys to ListSshKeys
    Change-Id: Ib266ef5cb5c78a8eb11150ae638e041bf07dda2d
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- gitee/users.go
+++ gitee/users.go
@@ -114,7 +114,7 @@ func (k SshKey) String() string {
 
 // 获取当前授权用户的sshkey，这个能获取多个的，一个列表
 // 列出授权用户的所有公钥 GET https://gitee.com/api/v5/user/keys
-func (s *UsersService) GetUserKeys(ctx context.Context, opts *ListOptions) ([]*SshKey, *Response, error) {
+func (s *UsersService) ListSshKeys(ctx context.Context, opts *ListOptions) ([]*SshKey, *Response, error) {
 	u, err := addOptions("user/keys", opts)
 	if err != nil {
 		return nil, nil, err
