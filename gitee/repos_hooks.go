8ac91c4854319283e172abeb59f293690db9929a
bright.ma <bright.ma@blackshark.com>
2022-11-16 09:59:11 +0800

    gitee: repos_hooks.go: add func ListHooks
    Change-Id: Ifcbf16321d3606dde8ded7929d6c566f266b86b8
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- gitee/repos_hooks.go
+++ gitee/repos_hooks.go
@@ -13,3 +13,50 @@
 //limitations under the License.
 
 package gitee
+
+import (
+	"context"
+	"fmt"
+)
+
+type Hook struct {
+	ID                  *int64     `json:"id,omitempty"`
+	URL                 *string    `json:"url,omitempty"`
+	CreatedAt           *Timestamp `json:"created_at,omitempty"`
+	Password            *string    `json:"password,omitempty"`
+	ProjectID           *int64     `json:"project_id,omitempty"`
+	Result              *string    `json:"result,omitempty"`
+	ResultCode          *int       `json:"result_code,omitempty"`
+	PushEvents          *bool      `json:"push_events,omitempty"`
+	TagPushEvents       *bool      `json:"tag_push_events,omitempty"`
+	IssuesEvents        *bool      `json:"issues_events,omitempty"`
+	NoteEvents          *bool      `json:"note_events,omitempty"`
+	MergeRequestsEvents *bool      `json:"merge_requests_events,omitempty"`
+}
+
+func (h Hook) String() string {
+	return Stringify(h)
+}
+
+// ListHooks lists all Hooks for the specified repository.
+//
+func (s *RepositoriesService) ListHooks(ctx context.Context, owner, repo string, opts *ListOptions) ([]*Hook, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/hooks", owner, repo)
+	u, err := addOptions(u, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var hooks []*Hook
+	resp, err := s.client.Do(ctx, req, &hooks)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return hooks, resp, nil
+}
