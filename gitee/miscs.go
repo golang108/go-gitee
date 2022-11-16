967b3fea1afcbdca1979b5253c6ec0e0f8d0b876
bright.ma <bright.ma@blackshark.com>
2022-11-16 10:54:35 +0800

    gitee: miscs.go: add func ListEmojis
    Change-Id: I59cbc471bb6a0a87a5f355969b3266b5ff0eac00
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- gitee/miscs.go
+++ gitee/miscs.go
@@ -14,5 +14,25 @@
 
 package gitee
 
+import "context"
+
 // MiscellaneousService 杂项
 type MiscellaneousService service
+
+// ListEmojis returns the emojis available to use on GitHub.
+//
+// 列出可使用的 Emoji GET https://gitee.com/api/v5/emojis
+func (s *MiscellaneousService) ListEmojis(ctx context.Context) (map[string]string, *Response, error) {
+	req, err := s.client.NewRequest("GET", "emojis", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var emoji map[string]string
+	resp, err := s.client.Do(ctx, req, &emoji)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return emoji, resp, nil
+}
