c0bd21080e41b7ebc2704bd888f46c6636895d5f
bright.ma <bright.ma@blackshark.com>
2022-11-16 12:42:28 +0800

    test: miscs_test.go: add TestMarkdown
    Change-Id: I5bdd70293c765559a1ededd5790a6dfa93047608
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- test/miscs_test.go
+++ test/miscs_test.go
@@ -2,11 +2,22 @@ package test
 
 import (
 	"fmt"
+	"github.com/mamh-mixed/go-gitee/gitee"
 	"testing"
 )
 
 func TestListEmojis(t *testing.T) {
 	rr, response, err := client.Miscellaneous.ListEmojis(ctx)
+	fmt.Println(rr)
+	fmt.Println(response)
+	fmt.Println(err)
+}
+
+func TestMarkdown(t *testing.T) {
+	opts := &gitee.MarkdownRequest{
+		Text: gitee.String("# xxxxxxxxxxxx test"),
+	}
+	rr, response, err := client.Miscellaneous.Markdown(ctx, opts)
 	fmt.Println(rr)
 	fmt.Println(response)
 	fmt.Println(err)
