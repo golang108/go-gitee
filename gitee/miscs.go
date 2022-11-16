326e03856051d17c506431143849407ca4177b46
bright.ma <bright.ma@blackshark.com>
2022-11-16 10:35:02 +0800

    gitee: miscs.go: add func License
    Change-Id: I9812c03bf57405a11e9db6378f3e07cd531072d5
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- gitee/miscs.go
+++ gitee/miscs.go
@@ -14,5 +14,38 @@
 
 package gitee
 
+import (
+	"context"
+	"fmt"
+)
+
 // MiscellaneousService 杂项
 type MiscellaneousService service
+
+// RepositoryLicense represents the license for a repository.
+type RepositoryLicense struct {
+	License *string `json:"license,omitempty"`
+}
+
+func (l RepositoryLicense) String() string {
+	return Stringify(l)
+}
+
+// License gets the contents of a repository's license if one is detected.
+// 这个接口github放到repos 里面了
+// 获取一个仓库使用的开源许可协议 GET https://gitee.com/api/v5/repos/{owner}/{repo}/license
+func (s *RepositoriesService) License(ctx context.Context, owner, repo string) (*RepositoryLicense, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/license", owner, repo)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	r := &RepositoryLicense{}
+	resp, err := s.client.Do(ctx, req, r)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return r, resp, nil
+}
